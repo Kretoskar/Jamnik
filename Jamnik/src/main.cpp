66c19a378e7b113d1f4453dbe2dc7e2bb998544e
Oskar Kogut <kogutoskar123@gmail.com>
2024-05-11 22:23:49 +0200

    imgui

--- Jamnik/src/main.cpp
+++ Jamnik/src/main.cpp
@@ -7,6 +7,11 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+
 int main()
 {
     glfwInit();
@@ -20,7 +25,7 @@ int main()
     vec = trans * vec;
     std::cout << vec.x << vec.y << vec.z << std::endl;
     
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Jamnik", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Jamnik", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -35,14 +40,40 @@ int main()
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
+    glViewport(320, 0, 1280, 720);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    const char* glslVersion = "#version 440 core";
+    ImGui_ImplOpenGL3_Init(glslVersion);
+
+    glClearColor(0.15f, 0.5f, 1.0f, 1.0f);
 
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGuiWindowFlags flags = 0;
+        ImGui::SetNextWindowBgAlpha(0.8f);
+        ImGui::Begin("chuj", nullptr, flags);
+        ImGui::Text("chasdasdasdasdasddauj");
+
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
