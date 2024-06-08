de3df0c86513cc2b6549781cabe235be597d4ac7
Oskar Kogut <kogutoskar123@gmail.com>
2024-06-08 16:34:08 +0200

    Add camera move input

--- Jamnik/src/Rendering/Camera.cpp
+++ Jamnik/src/Rendering/Camera.cpp
@@ -11,7 +11,10 @@ void Camera::SetVPMatricesInShader(Jamnik::Shader& shader)
 {
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
+
+    // TODO: delta time 
     
+    Position += Velocity;
     view = glm::lookAt(Position, Position + Orientation, Up);
 
     projection = glm::perspective(glm::radians(FOVdeg), static_cast<float>(width) / static_cast<float>(height), nearPlane, farPlane);
@@ -23,7 +26,23 @@ void Camera::SetVPMatricesInShader(Jamnik::Shader& shader)
 void Camera::Init()
 {
     JAMNIK_BIND_EVENT(MouseButtonEvent::Type(GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS), OnLeftMouseButtonClick)
+    
     JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_W, GLFW_PRESS), OnForwardPressed)
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_W, GLFW_RELEASE), OnForwardReleased)
+
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_S, GLFW_PRESS), OnBackwardPressed)
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_S, GLFW_RELEASE), OnBackwardReleased)
+
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_D, GLFW_PRESS), OnRightPressed)
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_D, GLFW_RELEASE), OnRightReleased)
+
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_A, GLFW_PRESS), OnLeftPressed)
+    JAMNIK_BIND_EVENT(KeyboardEvent::Type(GLFW_KEY_A, GLFW_RELEASE), OnLeftReleased)
+}
+
+glm::vec3 Camera::GetRightVector() const
+{
+    return glm::normalize(glm::cross(Orientation, Up));
 }
 
 void Camera::OnLeftMouseButtonClick(const Event& event)
@@ -33,5 +52,40 @@ void Camera::OnLeftMouseButtonClick(const Event& event)
 
 void Camera::OnForwardPressed(const Event& event)
 {
-    JAMNIK_LOG_WARNING("Button %i", dynamic_cast<const KeyboardEvent*>(&event)->GetKey())
+    Velocity += Orientation * speed;
+}
+
+void Camera::OnForwardReleased(const Event& event)
+{
+    Velocity -= Orientation * speed;
+}
+
+void Camera::OnBackwardPressed(const Event& event)
+{
+    Velocity += -Orientation * speed;
+}
+
+void Camera::OnBackwardReleased(const Event& event)
+{
+    Velocity -= -Orientation * speed;
+}
+
+void Camera::OnRightPressed(const Event& event)
+{
+    Velocity += GetRightVector() * speed;
+}
+
+void Camera::OnRightReleased(const Event& event)
+{
+    Velocity -= GetRightVector() * speed;
+}
+
+void Camera::OnLeftPressed(const Event& event)
+{
+    Velocity += -GetRightVector() * speed;
+}
+
+void Camera::OnLeftReleased(const Event& event)
+{
+    Velocity -= -GetRightVector() * speed;
 }
--- Jamnik/src/Rendering/Camera.h
+++ Jamnik/src/Rendering/Camera.h
@@ -18,21 +18,36 @@ class Camera
     int width;
     int height;
     
-    float speed = 0.1f;
+    float speed = 0.01f;
     float sensitivity = 100.0f;
 
     float FOVdeg = 120;
     float nearPlane = 0.1f;
     float farPlane = 1000;
+
+    glm::vec3 Velocity = glm::vec3(0.0f, 0.0f, 0.0f);
     
 public:
     Camera(int width, int height, glm::vec3 position)
         : width(width), height(height), Position(position) {}
     void Init();
 
+    glm::vec3 GetRightVector() const;
+    
     void OnLeftMouseButtonClick(const Event& event);
+    
     void OnForwardPressed(const Event& event);
+    void OnForwardReleased(const Event& event);
 
+    void OnBackwardPressed(const Event& event);
+    void OnBackwardReleased(const Event& event);
+
+    void OnRightPressed(const Event& event);
+    void OnRightReleased(const Event& event);
+
+    void OnLeftPressed(const Event& event);
+    void OnLeftReleased(const Event& event);
+    
     // Updates and exports the camera matrix to the Vertex Shader
     void SetVPMatricesInShader(Jamnik::Shader& shader);
 };
